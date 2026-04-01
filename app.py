a97ad970175ba0a44c7ed7757a333b85fe6290f3
yankopach13-create <yankopach13@gmail.com>
2026-04-01 11:56:21 +0300

    ыаиыа

--- app.py
+++ app.py
@@ -1304,15 +1304,11 @@ if uploaded_file_1 and uploaded_file_2:
         st.caption("Отбор кластеров для статистики по неделям.")
         cluster_options_only = [_cluster_display_name(c) for c in CLUSTER_8_ORDER] + ["Не покупали"]
         cluster_options_with_all = ["Все кластеры"] + cluster_options_only
-        if "lifecycle_clusters_selection" not in st.session_state:
-            st.session_state["lifecycle_clusters_selection"] = ["Все кластеры"]
         if "lifecycle_clusters_multiselect" not in st.session_state:
             st.session_state["lifecycle_clusters_multiselect"] = ["Все кластеры"]
-        default_lc = st.session_state["lifecycle_clusters_selection"]
         raw_selection = st.multiselect(
             "Кластеры",
             options=cluster_options_with_all,
-            default=default_lc,
             key="lifecycle_clusters_multiselect",
             label_visibility="collapsed",
         )
@@ -1321,7 +1317,6 @@ if uploaded_file_1 and uploaded_file_2:
             selected_clusters_lifecycle = [x for x in raw_selection if x != "Все кластеры"]
         else:
             selected_clusters_lifecycle = raw_selection if raw_selection else ["Все кластеры"]
-        st.session_state["lifecycle_clusters_selection"] = selected_clusters_lifecycle
         if raw_selection != selected_clusters_lifecycle:
             st.session_state["lifecycle_clusters_multiselect"] = selected_clusters_lifecycle
             st.rerun()
